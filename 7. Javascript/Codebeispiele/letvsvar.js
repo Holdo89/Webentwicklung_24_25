3e9ac9e3d001c2e9294996ccf831a547d06fe8f4
Holdo89 <d.holzweber@hotmail.com>
2024-11-26 18:26:11 +0100

    Grundlagen Slides PDF

--- 7. Javascript/Codebeispiele/letvsvar.js
+++ 7. Javascript/Codebeispiele/letvsvar.js
@@ -5,7 +5,7 @@
     let letVariable = 'This is let';
 }
 console.log(varVariable); // accessible outside the block
-// console.log(letVariable); // not accessible outside the block
+//console.log(letVariable); // not accessible outside the block
 
 //----------------------------------------------------------------------------
 
@@ -16,7 +16,7 @@ let letVariable = 'Initial value';
 // Redeclaring the variables
 var varVariable = 'New value'; // No error
 //let letVariable = 'New value'; // Error: SyntaxError: Identifier 'letVariable' has already been declared
-
+console.log(varVariable)
 //----------------------------------------------------------------------------
 
 
@@ -24,24 +24,24 @@ var varVariable = 'New value'; // No error
 console.log(hoistedVar); // undefined
 var hoistedVar = 'I am hoisted';
 
-// console.log(hoistedLet); // ReferenceError: Cannot access 'hoistedLet' before initialization
+//console.log(hoistedLet); // ReferenceError: Cannot access 'hoistedLet' before initialization
 let hoistedLet = 'I am not hoisted';
 
 //----------------------------------------------------------------------------
 
 
 // Example 4: Scope in a loop
-// for (var i = 0; i < 3; i++) {
-//     setTimeout(function() {
-//         console.log('The value of i with var is: ' + i); // Prints 3 three times
-//     }, 1000);
-// }
+for (var i = 0; i < 3; i++) {
+    setTimeout(function() {
+        console.log('The value of i with var is: ' + i); // Prints 3 three times
+    }, 1000);
+}
 
-// for (let j = 0; j < 3; j++) {
-//     setTimeout(function() {
-//         console.log('The value of j with let is: ' + j); // Prints 0, 1, 2
-//     }, 1000);
-// }
+for (let j = 0; j < 3; j++) {
+    setTimeout(function() {
+        console.log('The value of j with let is: ' + j); // Prints 0, 1, 2
+    }, 1000);
+}
 
 
 
