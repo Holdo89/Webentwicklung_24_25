ff9f39f513bdbc04e0ce6ada49a551b394b31ec4
Merso <mbesic@gmx.at>
2024-12-03 18:59:29 +0100

    merso

--- 0. Eure Codebeispiele/Mersad/05.Javascript/verzweigungenEXC.js
+++ 0. Eure Codebeispiele/Mersad/05.Javascript/verzweigungenEXC.js
@@ -1,56 +1,52 @@
 // Beispiel:
-let name = prompt("Bitte Namen eingeben");
-let nameLowerCase = name.toLowerCase();
-let lastIndexOfSpace = name.lastIndexOf(" ");
-let vorname = name[0].toUpperCase() + nameLowerCase.slice(1, lastIndexOf);
-let nachname =
-  name[lastIndexOfSpace + 1].toUpperCase() +
-  nameLowerCase.slice(lastIndexOfSpace + 2);
-alert("Mein Name schön formatiert: " + vorname + " " + nachname);
+// let name = prompt("Bitte Namen eingeben");
+// let nameLowerCase = name.toLowerCase();
+// let lastIndexOfSpace = name.lastIndexOf(" ");
+// let vorname = name[0].toUpperCase() + nameLowerCase.slice(1, lastIndexOf);
+// let nachname =
+//  name[lastIndexOfSpace + 1].toUpperCase() +
+//  nameLowerCase.slice(lastIndexOfSpace + 2);
+//prompt("Mein Name schön formatiert: " + vorname + " " + nachname);
 
 // Beispiel 1:
-let Temperatur = prompt("Gib bitte die Temperatur ein");
-if (Temperatur < 10) {
-  console.log("Es ist kalt");
-} else if (Temperatur > 10 && Temperatur < 25) console.log("Es ist angenehm");
-else {
-  console.log("Es ist heiß");
-}
+// let Temperatur = prompt("Gib bitte die Temperatur ein");
+// if (Temperatur < 10) {
+// prompt("Es ist kalt");
+// } else if (Temperatur > 10 && Temperatur < 25) prompt("Es ist angenehm");
+// else {
+// prompt("Es ist heiß");
+//}
 
 // Beispiel 2:
-let Buchstabe = prompt("Bitte gib einen Buchstaben ein");
-switch (Buchstabe) {
-  case "a":
-  case "e":
-  case "i":
-  case "o":
-  case "u":
-    console.log("Buchstabe ist ein Vokal");
-    break;
-  default:
-    console.log("Buchstabe ist ein Konsonant");
-}
+// let Buchstabe = prompt("Bitte gib einen Buchstaben °ein");
+// switch (Buchstabe) {
+//  case "a":
+//  case "e":
+//  case "i":
+//  case "o":
+//  case "u":
+//    prompt("Buchstabe ist ein Vokal");
+//    break;
+//  default:
+//    prompt("Buchstabe ist ein Konsonant");
+//}
 
 // Beispiel 3:
-let Zahl1 = prompt("Bitte gib eine Zahl ein");
-let Zahl2 = prompt("Bitte gib eine Zahl ein");
-let Zahl3 = prompt("Bitte gib eine Zahl ein");
+// let Zahl1 = prompt("Bitte gib eine Zahl ein");
+// let Zahl2 = prompt("Bitte gib eine Zahl ein");
+// let Zahl3 = prompt("Bitte gib eine Zahl ein");
 
-const maximum =
-  zahl1 > zahl2 && zahl1 > zahl3 ? zahl2 > zahl1 && zahl2 > zahl3 : zahl3;
+//const maximum =
+//  zahl1 > zahl2 && zahl1 > zahl3 ? zahl2 > zahl1 && zahl2 > zahl3 : zahl3;
 
-console.log("die größe Zahl: " + maximum);
+// prompt("die größe Zahl: " + maximum);
 
 // Beispiel 4:
-let userZahl = prompt("Bitte gib eine Zahl ein");
-if (userZahl % 2 == 0) {
-  console.log("Es ist eine gerade Zahl");
-} else if (userZahl % 2 == 1) {
-  console.log("Die Zahl ist ungerade");
-} else {
-  console.log("Es ist eine Kommazahl");
-}
-
-let UserZahl1 = prompt("Bitte eine Zahl eingeben");
-let UserZahl2 = prompt("Bitte gib die zweite Zahl ein");
-let UserZahl3 = prompt("Bitte gib die dritte Zahl ein ");
+// let userZahl = prompt("Bitte gib eine Zahl ein");
+// if (userZahl % 2 == 0) {
+//  prompt("Es ist eine gerade Zahl");
+// } else if (userZahl % 2 == 1) {
+//  prompt("Die Zahl ist ungerade");
+// } else {
+//  prompt("Es ist eine Kommazahl");
+// }
