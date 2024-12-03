77e9315e5ef5d4a0f786cb7740f42729046c97b2
Hasan_Gudic <hasangdc@icloud.com>
2024-12-03 20:04:57 +0100

    powHasaan

--- 0. Eure Codebeispiele/Hasan/JSuebung/FunktionUebung/pow.js
+++ 0. Eure Codebeispiele/Hasan/JSuebung/FunktionUebung/pow.js
@@ -8,8 +8,15 @@ function pow (a, b) {
 }
 pow(4);
 
+//ohne if-else
+
 function pow(a, b = 2){
     console.log(a ** b)
 }
 pow(3,4);
 pow(3);
+
+//addArrow
+
+const addArrow = (a, b = 2) => a ** b;
+console.log(addArrow(2, 8));
