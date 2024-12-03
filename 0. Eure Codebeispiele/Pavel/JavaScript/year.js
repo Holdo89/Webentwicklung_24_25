12f10af64f894a7da9d54f2939785799772dd77d
Pavel Grega <pavel.grega01@outlook.sk>
2024-12-03 20:36:20 +0100

    year

--- 0. Eure Codebeispiele/Pavel/JavaScript/year.js
+++ 0. Eure Codebeispiele/Pavel/JavaScript/year.js
@@ -1,12 +1,12 @@
 function Schaltjahr(year) {
-    if ((year % 4 == 0 && year % 100 !== 0) || year % 400 ==0 ) {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 ==0 ) {
         return true;
     } else {
         return false;
     }
 
 }
-let year = 2023
+let year = 2024
 
 if (Schaltjahr(year)) {
     console.log(year + " ist ein Schaltjahr.");
