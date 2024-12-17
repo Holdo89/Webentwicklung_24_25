cc7b0ec3ac0cb199823a2e4cbe1b0fb01f9cff89
Merso <mbesic@gmx.at>
2024-12-17 20:38:40 +0100

    merso

--- 0. Eure Codebeispiele/Mersad/05.Javascript/fibonacci.js
+++ 0. Eure Codebeispiele/Mersad/05.Javascript/fibonacci.js
@@ -22,17 +22,15 @@ generateFibonacci2(10);
 
 function generateFibonacci2(wert) {
   let array = [wert];
-  let zahlen = 0;
   let a = 0;
   let b = 1;
   let ergebnis;
   let index = 0;
-  while (zahlen < wert) {
+  while (index < wert) {
     array[index] = a;
     ergebnis = a + b;
     a = b;
     b = ergebnis;
-    zahlen++;
     index++;
   }
   console.log(array);
