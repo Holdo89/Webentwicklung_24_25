c7449c6608070a172b72fb9e246f0e20c6ed4e04
Merso <mbesic@gmx.at>
2024-12-17 20:36:53 +0100

    mersad

--- 0. Eure Codebeispiele/Mersad/05.Javascript/arrayMethodsRanking.js
+++ 0. Eure Codebeispiele/Mersad/05.Javascript/arrayMethodsRanking.js
@@ -1,20 +1,60 @@
+const { forEach } = require("async");
+
 const participants = [
-    { name: 'Alice', points: 120 },
-    { name: 'Bob', points: 80 },
-    { name: 'Charlie', points: 95 },
-    { name: 'David', points: 110 },
-    { name: 'Eva', points: 60 }
-  ];
+  { name: "Alice", points: 120 },
+  { name: "Bob", points: 80 },
+  { name: "Charlie", points: 95 },
+  { name: "David", points: 110 },
+  { name: "Eva", points: 60 },
+];
 
 //   Aufgaben:
 // Verwende die forEach-Methode, um jeden Teilnehmer und seine Punktzahl auszugeben.
 
+console.log("alle Teilnehmer: ");
+participants.forEach(myFunction);
+function myFunction(element) {
+  console.log(element);
+}
+console.log("Neue Zeile: ");
+
 // Verwende die map-Methode, um ein neues Array zu erstellen, das nur die Namen der Teilnehmer enthält.
+console.log("nur Namen: ");
+const nurNamen = participants.map((element) => element.name);
+console.log(nurNamen);
+console.log("Neue Zeile: ");
 
 // Verwende die filter-Methode, um ein neues Array zu erstellen, das nur die Teilnehmer enthält, die mehr als 100 Punkte haben.
+console.log("alle über 100 Punkten: ");
+const filteredArray = participants.filter((element) => element.points > 100);
+filteredArray.forEach(myFunction);
+function myFunction(element) {
+  console.log(element);
+}
+console.log("Neue Zeile: ");
 
 // Verwende die sort-Methode, um die Liste der Teilnehmer nach ihren Punktzahlen zu sortieren (aufsteigend).
+console.log("aufsteigend sortiert: ");
+const sort = participants.sort((a, b) => a.points - b.points);
+sort.forEach(myFunction);
+function myFunction(element) {
+  console.log(element);
+}
+console.log("Neue Zeile: ");
 
 // Verwende die reverse-Methode, um die Liste in absteigender Reihenfolge zu sortieren.
+console.log("absteigend sortiert: ");
+const sort2 = participants.sort((a, b) => b.points - a.points);
+sort2.forEach(myFunction);
+function myFunction(element) {
+  console.log(element);
+}
+console.log("Neue Zeile: ");
 
 // Gib die sortierte Rangliste aus, die sowohl den Namen als auch die Punktzahl jedes Teilnehmers enthält.
+console.log("aufsteigend sortiert mit Punkten");
+const sort3 = participants.sort((a, b) => a.points - b.points);
+sort3.forEach(myFunction);
+function myFunction(element) {
+  console.log(element.name + " " + element.points);
+}
--- 0. Eure Codebeispiele/Mersad/05.Javascript/fibonacci.js
+++ 0. Eure Codebeispiele/Mersad/05.Javascript/fibonacci.js
@@ -1,3 +1,39 @@
 // Erstelle eine Funktion die die Fibonacci-Folge bis zu einem bestimmten Wert generiert
 // Beispiel: generateFibonacci(10)
 // Output: 0,1,1,2,3,5,8,13,21,34
+
+generateFibonacci(10);
+
+function generateFibonacci(wert) {
+  let zahlen = 0;
+  let a = 0;
+  let b = 1;
+  let ergebnis;
+  while (zahlen < wert) {
+    ergebnis = a + b;
+    console.log(a);
+    a = b;
+    b = ergebnis;
+    zahlen++;
+  }
+}
+
+generateFibonacci2(10);
+
+function generateFibonacci2(wert) {
+  let array = [wert];
+  let zahlen = 0;
+  let a = 0;
+  let b = 1;
+  let ergebnis;
+  let index = 0;
+  while (zahlen < wert) {
+    array[index] = a;
+    ergebnis = a + b;
+    a = b;
+    b = ergebnis;
+    zahlen++;
+    index++;
+  }
+  console.log(array);
+}
