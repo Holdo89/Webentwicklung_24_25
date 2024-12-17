9877e1158ef9cf4b880c79335f78ffb736132ea0
Ayham <a.kassab80@gmail.com>
2024-12-17 21:24:23 +0100

    commits23

--- 0. Eure Codebeispiele/Ayham/JS/fizzbuzz.js
+++ 0. Eure Codebeispiele/Ayham/JS/fizzbuzz.js
@@ -4,3 +4,38 @@
 // Zahlen die durch 3 und 5 teilbar sind werden durch „FizzBuzz“ ersetzt.
 // Nutze dafür eine while loop!
 // Beispiel Output: 1,2,Fizz, 4,Buzz,Fizz,7,8,….
+
+
+let x = 100;
+while (x > 0){
+    if(x % 3 == 0 && x % 5 == 0)
+        console.log('„FizzBuzz“')
+    else if(x % 5 == 0)
+        console.log('„Buzz“')
+    else if(x % 3 == 0)
+        console.log('„Fizz“')
+    else
+        console.log(x)
+    x--;
+}
+
+let beer = 12;
+for(let i = beer; i > 0 ; i--){
+    console.log(i + ' botteles of beer on the wall,'+ i + ' botteles of beer')
+    console.log('Take one down and pass it around, '+ (i-1) + ' bottles of beeer on the wall' )
+}
+
+function fib(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    const fibArray = [0, 1]; 
+    for (let i = 2; i <= n; i++) {
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    return fibArray
+}
+
+console.log(fib(10));
+
+
