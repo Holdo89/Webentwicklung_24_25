a9d0e7dbb3fa202c98c9716716bc2d0abb5202b5
Holdo89 <d.holzweber@hotmail.com>
2025-01-21 18:05:19 +0100

    Add initial files and structure for Drumkit project

--- 7. Javascript/Codebeispiele/Dices/dice.js
+++ 7. Javascript/Codebeispiele/Dices/dice.js
@@ -3,7 +3,9 @@ const firstDice = document.querySelector(".img1");
 const secondDice = document.querySelector(".img2");
 const header = document.querySelector("h1");
 
-function newGame() {
+function newGame(event) {
+  event.stopPropagation();
+  console.log(event);
   gameButton.style.backgroundColor = "green";
   let randomDice1 = Math.floor(Math.random() * 6) + 1;
   firstDice.setAttribute("src", `./images/dice${randomDice1}.png`);
@@ -18,4 +20,17 @@ function newGame() {
     header.innerText = "Draw";
   }
 }
+document.addEventListener("keydown", function (event) {
+  if (event.key === "n") {
+    newGame();
+  } else if (event.key === "r") {
+    prompt("Reload the page");
+  } else {
+    console.log(event);
+  }
+});
+
+document.addEventListener("click", function (event) {
+  alert("Click anywhere to roll the dice");
+});
 gameButton.onclick = newGame;
