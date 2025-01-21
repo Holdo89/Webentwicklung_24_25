50b5275f513d5f023a5aad82e2ed2bb3a7cc90f2
Merso <mbesic@gmx.at>
2025-01-21 19:19:31 +0100

    merso

--- 7. Javascript/Codebeispiele/Drumkit/index.js
+++ 7. Javascript/Codebeispiele/Drumkit/index.js
@@ -1,1 +1,31 @@
-//Todo
+document.addEventListener("keydown", function (event) {
+  playSound(sound);
+
+  //  if (event.key === "w") {
+  //    let sound = new Audio("sounds/" + event.key + ".mp3");
+  //    sound.play();
+  //  } else if (event.key === "a") {
+  //    let sound = new Audio("sounds/" + event.key + ".mp3");
+  //    sound.play();
+  //  } else if (event.key === "s") {
+  //    let sound = new Audio("sounds/" + event.key + ".mp3");
+  //    sound.play();
+  //  } else if (event.key === "d") {
+  //    let sound = new Audio("sounds/" + event.key + ".mp3");
+  //    sound.play();
+  //  } else if (event.key === "j") {
+  //    let sound = new Audio("sounds/" + event.key + ".mp3");
+  //    sound.play();
+  //  } else if (event.key === "k") {
+  //    let sound = new Audio("sounds/" + event.key + ".mp3");
+  //    sound.play();
+  //  } else if (event.key === "l") {
+  //    let sound = new Audio("sounds/" + event.key + ".mp3");
+  //    sound.play();
+  //  }
+});
+
+function playSound(sound) {
+  let Sound = new Audio("sounds/" + sound.key + ".mp3");
+  Sound.play();
+}
