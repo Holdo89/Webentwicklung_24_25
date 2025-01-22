22a151e3b39c5f9c3ec4480dc3e8e554dedf818e
Ayham <a.kassab80@gmail.com>
2025-01-22 08:50:55 +0100

    drum

--- 0. Eure Codebeispiele/Ayham/JS/Drumkit/index.js
+++ 0. Eure Codebeispiele/Ayham/JS/Drumkit/index.js
@@ -1,1 +1,16 @@
 //Todo
+
+document.addEventListener('keydown', function(drum){
+    let p = new Audio('./sounds/'+drum.key+'.mp3');
+    p.play();
+});
+const elements = document.querySelectorAll('.drum');
+for(let element of elements) {
+    element.addEventListener('click', function() {
+        const text = element.textContent;
+        let pS = new Audio('./sounds/'+text+'.mp3');
+        pS.play();
+    });
+};
+
+
