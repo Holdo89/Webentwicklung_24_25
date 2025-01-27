d095471fdb21dd42355abf54b859bd8b345edfd0
Ayham <a.kassab80@gmail.com>
2025-01-27 11:12:19 +0100

    aufg

--- 0. Eure Codebeispiele/Ayham/JS/Übungsbeispiele/leicht.js
+++ 0. Eure Codebeispiele/Ayham/JS/Übungsbeispiele/leicht.js
@@ -1,14 +1,45 @@
 // 1. Schreibe eine Funktion, die die Summe von zwei Zahlen zurückgibt.
+function sum(n1,n2=0){
+    return n1+n2;
+}
+console.log(sum(2,5));
 
 // 2. Schreibe eine Funktion, die überprüft, ob eine Zahl gerade oder ungerade ist.
+function gerade(n){
+    n % 2 == 0 ? console.log('Es ist gerade Zhal') :console.log('Es ist ungerade Zhal')
+}
+gerade(6);
 
 // 3. Schreibe eine Funktion, die einen String umkehrt.
-
+function umkehrt(str){
+    return str.split('').reverse().join('');
+}
+console.log(umkehrt('Hello'));
 // 4. Schreibe eine Funktion, die die größte Zahl in einem Array findet.
-
+function maxi(arr){
+    return console.log(Math.max(...arr));
+}
+maxi([1,2,0,3,8,4,9]);
 // 5. Schreibe eine Funktion, die die Anzahl der Vokale in einem String zählt.
+function coun(str){
+    const char = ['A','E','I','O','U'];
+    let count = 0;
+    let upp = str.toUpperCase();
+    let arr = upp.split('');
+    for (const v of arr) {
+        if(char.includes(v))
+        count++;
+    }
+    return console.log(count);
+}
+coun('Hello');
 
 // 6. Schreibe eine Funktion, die überprüft, ob ein String ein Palindrom ist.
+function pal(str){
+    const pl1 = umkehrt(str);
+    return str == pl1 ? true : false;
+}
+console.log(pal('mam'));
 
 // 7. Schreibe eine Funktion, die ein Array von Zahlen in aufsteigender Reihenfolge sortiert.
 
