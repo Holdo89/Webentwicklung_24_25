4e782fbfb47e51003bec75c1e2ee3a756bf8c92b
Ayham <a.kassab80@gmail.com>
2025-02-18 18:15:00 +0100

    erewrewrewr

--- 0. Eure Codebeispiele/Ayham/NodeJS/Server.js
+++ 0. Eure Codebeispiele/Ayham/NodeJS/Server.js
@@ -1,1 +1,6 @@
 import express from "express";
+let app = express();
+
+app.get('/', (req,res) =>{res.send("<h3>WELLCOM BACK</h3>")});
+app.get("/about", (req,res) =>{res.send("<h3>ABOUT US</h3>")});
+app.listen(8090, () => {console.log("SERVER IS RUNNING")});
