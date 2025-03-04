43ecf14afa57149e3bdb19d16157a3209b5e8de2
Thomas Meindlhumer <tom77654@example.com>
2025-03-04 17:38:52 +0100

    aa

--- 7. Javascript/Übungsbeispiele/Neu/übungen.js
+++ 7. Javascript/Übungsbeispiele/Neu/übungen.js
@@ -1,20 +1,41 @@
 // Schreibe eine Funktion, die zwei Zahlen multipliziert.
+function two (a,b){
+    return a*b;
+}
 // Schreibe eine Funktion, die prüft, ob eine Zahl negativ ist.
+    
 // Schreibe eine Funktion, die den ersten Buchstaben eines Strings zurückgibt.
+
 // Schreibe eine Funktion, die prüft, ob ein String leer ist.
+
 // Schreibe eine Funktion, die das Quadrat einer Zahl berechnet.
+
 // Schreibe eine Funktion, die die letzte Zahl eines Arrays zurückgibt.
+
 // Schreibe eine Funktion, die prüft, ob eine Zahl durch 5 teilbar ist.
+
 // Schreibe eine Funktion, die eine Zahl in eine Zeichenkette umwandelt.
+
 // Schreibe eine Funktion, die prüft, ob ein Array ein bestimmtes Element enthält.
+
 // Schreibe eine Funktion, die zwei Strings miteinander verbindet.
+
 // Schreibe eine Funktion, die eine Zahl verdoppelt.
+
 // Schreibe eine Funktion, die prüft, ob ein Array leer ist.
+
 // Schreibe eine Funktion, die ein Array mit einer bestimmten Anzahl an gleichen Zahlen füllt.
+
 // Schreibe eine Funktion, die prüft, ob eine Zahl größer als 100 ist.
+
 // Schreibe eine Funktion, die die Anzahl der Zeichen in einem String zählt.
+
 // Schreibe eine Funktion, die die kleinste Zahl in einem Array findet.
+
 // Schreibe eine Funktion, die ein Array mit Zahlen von 1 bis n füllt.
+
 // Schreibe eine Funktion, die das erste Wort eines Satzes zurückgibt.
+
 // Schreibe eine Funktion, die ein Array um eine bestimmte Anzahl an Stellen nach rechts verschiebt.
+
 // Schreibe eine Funktion, die die Summe aller geraden Zahlen in einem Array berechnet.
