e19ebdef1ec23f4066b3ef6926c40179e9ba9081
Marko Cankovic <marko.cankovic00@gmail.com>
2025-03-04 20:22:37 +0100

    testdone

---
+++ 0. Eure Codebeispiele/Marko/Testangaben/Testbeispiel.js
@@ -0,0 +1,19 @@
+//1. Schreibe eine JavaScript-Funktion, die die Summe von zwei Zahlen berechnet und das Ergebnis in der Konsole ausgibt. (2 Punkte)
+let a=2;
+let b=3;
+let c=0;
+
+function sumNumber(a, b){
+    c = a+b;
+    return c;
+}
+
+console.log(sumNumber(a, b));
+
+//2. Bonus: Schreibe eine JavaScript-Funktion, die beim Klick auf den Button aufgerufen wird und eine im Textfeld eingegebene Zahl *2 nimmt und das Ergebnis in der Konsole ausgibt. (4 Punkte) ------2 PUNKTE!!! NICHT VERGESSEN
+
+document.getElementById("myButton").addEventListener("click", function() {
+    let a=2;
+
+    console.log(a*2);
+})
