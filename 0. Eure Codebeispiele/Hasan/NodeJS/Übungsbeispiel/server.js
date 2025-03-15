7bd05d2bd0284a5518718d9fe95e45a70335789f
Hasan_Gudic <hasangdc@icloud.com>
2025-03-15 11:21:24 +0100

    serverHasan

--- 0. Eure Codebeispiele/Hasan/NodeJS/Übungsbeispiel/server.js
+++ 0. Eure Codebeispiele/Hasan/NodeJS/Übungsbeispiel/server.js
@@ -2,9 +2,27 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(checkCredentials);
+
+function checkCredentials(req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (password === "HelloBoy" && username === "Hasan") {
+        res.send ("Password is correct!");
+        next();
+    } else {
+        res.status(400).send("Wrong password");
+    }
+}
+
+
+
 app.get("/",(req, res) => {
     res.send("<h1>Home Page</h1>"); 
 });
+
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
