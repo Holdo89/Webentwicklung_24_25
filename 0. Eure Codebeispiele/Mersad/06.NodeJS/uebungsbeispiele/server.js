61635d806bdf9a5b2062fe988b7bcbf855bac551
Merso <mbesic@gmx.at>
2025-03-15 11:00:32 +0100

    merso

--- 0. Eure Codebeispiele/Mersad/06.NodeJS/uebungsbeispiele/server.js
+++ 0. Eure Codebeispiele/Mersad/06.NodeJS/uebungsbeispiele/server.js
@@ -2,13 +2,27 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passwordchecker);
+app.use(usernamechecker);
+
 app.get("/", (req, res) => {
   res.send("<h1> Mersad");
 });
 
-app.use(passwortchecker);
+function passwordchecker(req, res, next) {
+  const password = "Mersad";
+  const username = "Mersad";
 
-function passwortchecker(req, res, next) {}
+  if (req.body.password === password && req.body.username === username) {
+    next();
+  } else if (req.body.username !== username) {
+    res.status(400).send("username incorrect");
+  } else {
+    res.status(400).send("password incorrect");
+  }
+}
 
 app.listen(port, () => {
   console.log(`Server startet on port ${port}`);
