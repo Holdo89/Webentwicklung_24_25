df83a85e1a79e8ccceb5db0d0832d79d2d921142
Merso <mbesic@gmx.at>
2025-03-15 11:11:33 +0100

    merso

--- 0. Eure Codebeispiele/Mersad/06.NodeJS/uebungsbeispiele/server.js
+++ 0. Eure Codebeispiele/Mersad/06.NodeJS/uebungsbeispiele/server.js
@@ -5,22 +5,23 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passwordchecker);
-app.use(usernamechecker);
 
 app.get("/", (req, res) => {
-  res.send("<h1> Mersad");
+  res.send("<h1> Welcome User </h1>");
 });
 
 function passwordchecker(req, res, next) {
-  const password = "Mersad";
-  const username = "Mersad";
+  const password = "password";
+  const username = "mersad";
 
   if (req.body.password === password && req.body.username === username) {
     next();
   } else if (req.body.username !== username) {
     res.status(400).send("username incorrect");
-  } else {
+  } else if (req.body.password !== password) {
     res.status(400).send("password incorrect");
+  } else {
+    res.status(400).send("username and password incorrect");
   }
 }
 
