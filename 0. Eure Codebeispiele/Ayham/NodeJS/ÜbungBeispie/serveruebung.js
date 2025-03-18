95e6a84ff0ce2cf86b5d4d8ad8ee5f21e6d75dd7
Ayham <a.kassab80@gmail.com>
2025-03-18 18:01:15 +0100

    Jokes

--- 0. Eure Codebeispiele/Ayham/NodeJS/ÜbungBeispie/serveruebung.js
+++ 0. Eure Codebeispiele/Ayham/NodeJS/ÜbungBeispie/serveruebung.js
@@ -1,10 +1,55 @@
+/*import axios from 'axios';
+import express from 'express';
+
+let app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(checkPass);
+
+app.get("/5", (req, res) => {
+    res.send("<div>TEST</div>");
+});
+
+
+
+app.get('/iss', (req, res) => {
+    const url =  "https://api.wheretheiss.at/v1/satellites/25544";
+    try {
+        axios.get(url).then((response) => {
+            res.send(`<iframe
+            width="50%%"
+            height="600"
+            frameborder="0"
+            scrolling="no"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?height=400&hl=en&q=${response.data.latitude},${response.data.longitude}&t=&z=12&ie=UTF8&iwloc=B&output=embed"
+            ></iframe>`);
+        });
+    }
+    catch (error) {
+        res.send('ERROR');
+    }
+    
+});
+function checkPass(req,res,next){
+    let pass = req.body.password;
+    let user = req.body.username;
+    let password = '123456';
+    let name = 'Ayham';
+    pass != password && user != name ? res.status(404).send('Ops') : next();
+};
+
+app.listen(8090, () => {
+    console.log('STARTET');
+});*/
 import express from "express";
 import bodyParser from "body-parser";
-import { dirname } from "path";
+import { dirname, parse } from "path";
 import { fileURLToPath } from "url";
 import cors from "cors"
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
@@ -24,11 +69,18 @@ app.get("/random", (req, res) => {
 
 
 //Get a specific joke
-app.get("jokes/:id",(req,res) =>{
-  
-})
+app.get("/jokes/:id",(req,res) =>{
+  const id = parseInt(req.params.id);
+  const joke = jokes.find((el)=>el.id === id);
+  res.json(joke);
+});
 
 //Filter jokes by type
+app.get("/filter",(req,res) =>{
+  const type = parseInt(req.query.type);
+  const joke = jokes.filter((el)=>el.type === type);
+  res.json(joke);
+});
 
 //4. POST a new joke
 
