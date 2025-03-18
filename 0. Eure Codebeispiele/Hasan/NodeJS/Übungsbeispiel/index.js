8de85cc3722e530f750d79466460a69641311f79
Hasan_Gudic <hasangdc@icloud.com>
2025-03-18 21:57:29 +0100

    serverHasan

--- 0. Eure Codebeispiele/Hasan/NodeJS/Übungsbeispiel/index.js
+++ 0. Eure Codebeispiele/Hasan/NodeJS/Übungsbeispiel/index.js
@@ -1,13 +1,26 @@
-fetch("http://localhost:3000/jokes/50", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // andere Variante
-      //document.getElementById("demo").innerHTML = data.jokeText;
-      htmlElement.innerHTML = data.jokeText;
-      htmlElement.style.backgroundColor = "red";
-    });
+// andere Variante
+//document.getElementById("demo").innerHTML = "Hello World!";
+
+let htmlElement = document.getElementById("demo");
+
+fetch("http://localhost:3000/jokes/101", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    // andere Variante
+    //document.getElementById("demo").innerHTML = data.jokeText;
+    htmlElement.innerHTML = data.jokeText;
+    htmlElement.style.backgroundColor = "red";
+  });
+
+fetch("http://localhost:3000/jokes", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ id: 101, jokeText: "witzig", jokeType: "codersbay" }),
+});
