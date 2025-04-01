a3401a9aa7eecb2eb2aade176bceb73fe2466373
Hasan_Gudic <hasangdc@icloud.com>
2025-04-01 19:18:07 +0200

    issH

--- 0. Eure Codebeispiele/Hasan/NodeJS/Übungsbeispiel/iss.js
+++ 0. Eure Codebeispiele/Hasan/NodeJS/Übungsbeispiel/iss.js
@@ -1,4 +1,4 @@
-function getIssLocation() {
+//function getIssLocation() {
     fetch("http://localhost:3000/endpoint", {
       method: "GET",
       headers: { "Content-Type": "application/json" },
