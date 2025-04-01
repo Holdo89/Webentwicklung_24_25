d9b972696d7bdef688a3591ba2cd1cfacfb20980
Thomas Meindlhumer <tom77654@example.com>
2025-04-01 16:37:12 +0200

    123

---
+++ 0. Eure Codebeispiele/Thomas/html/Übungen.html/toDoList2/BackendToDoLIst/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+const port = 3000;
+import mysql from "mysql";
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const connection =mysql.createConnection({
+    host:'localhost',
+    user: "root",
+    password:"",
+    database:"todolist"
+});
+
+connection.connect((err)=>{
+    if(err){
+        console.error("Fehler bei der Verbindung:",err);
+    }else{
+        console.log("Erfolgreich mit der Datenbank verbunden");
+    }
+});
+
+
+
+
+
+app.get("/toDo",(req,res)=>{
+    const query="SELECT * FROM todolist";
+    connection.query(query,(error,results)=>{
+        if(error){
+            res.status(500).send("interner Serverfehler");
+        }else{
+            res.json(results);
+        }
+    });  
+    
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log("Server startet ok");
+  });
