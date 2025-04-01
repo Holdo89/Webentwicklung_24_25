d3db20b34c8672f6a21eb96c8a6f85a470861a21
Merso <mbesic@gmx.at>
2025-04-01 18:24:25 +0200

    merso

--- 0. Eure Codebeispiele/Mersad/06.NodeJS/uebungsbeispiele/iss.js
+++ 0. Eure Codebeispiele/Mersad/06.NodeJS/uebungsbeispiele/iss.js
@@ -1,10 +1,25 @@
-const { response } = require("express");
-
 function getIssLocation() {
-  fetch("https://api.wheretheiss.at/v1/satellites/25544", {
+  fetch("http://localhost:3000/endpoint", {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   })
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      const longitude = data.longitude;
+      const latitude = data.latitude;
+      document.getElementById("issLocation").innerHTML = `
+          Longitude: ${longitude} <br>
+          Latitude: ${latitude} <br>
+          <iframe
+            width="50%"
+            height="600"
+            frameborder="0"
+            scrolling="no"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?height=400&hl=en&q=${latitude},${longitude}&t=&z=12&ie=UTF8&iwloc=B&output=embed"
+          ></iframe>`;
+    })
+    .catch((error) =>
+      console.error("Fehler beim Abrufen der ISS-Daten:", error)
+    );
 }
