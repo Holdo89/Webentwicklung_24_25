a85f47d1094c89c47ce86a8db1bb8c0e985de92f
Hasan_Gudic <hasangdc@icloud.com>
2025-04-19 11:35:26 +0200

    präsi

--- 0. Eure Codebeispiele/Hasan/couchDB/app.js
+++ 0. Eure Codebeispiele/Hasan/couchDB/app.js
@@ -15,3 +15,33 @@ const db = nano.db.use('tiere');
 
   
 })();
+
+
+  (async () => {
+    try {
+      const newTier = {
+        tierart: 'Elefant',
+        tiername: 'Dumbo'
+      };
+  
+      const response = await db.insert(newTier);  
+      console.log(`Neues Tier gespeichert: ${response.id}`);
+    } catch (error) {
+      console.error('Fehler beim Hinzufügen:', error);
+    }
+  })();
+  
+
+  
+  (async () => {
+    try {
+      const docId = '61f7256776ef2a020c472c445c00c8f1'; 
+      const doc = await db.get(docId); 
+  
+     
+      const response = await db.destroy(doc._id, doc._rev);
+      console.log(`🗑️ Dokument mit ID "${docId}" wurde gelöscht.`);
+    } catch (error) {
+      console.error('❌ Fehler beim Löschen des Dokuments:', error);
+    }
+  })();
