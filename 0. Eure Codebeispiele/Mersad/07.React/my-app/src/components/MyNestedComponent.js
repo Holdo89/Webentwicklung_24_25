f064ffabbad3803b02e4173d47d023377124b497
Merso <mbesic@gmx.at>
2025-04-22 20:16:52 +0200

    merso

--- 0. Eure Codebeispiele/Mersad/07.React/my-app/src/components/MyNestedComponent.js
+++ 0. Eure Codebeispiele/Mersad/07.React/my-app/src/components/MyNestedComponent.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
+import myImage from "../components/IQRA.jpg";
 
 export default function MyNestedComponent(props) {
   const name = "Mersad";
   const message = `Hallo ${name}`;
   return (
     <div style={{ backgroundColor: "grey", color: props.color }}>
+      <img className="iqra" src={myImage} />
       Ich bin eine verschachtelte Komponente
       <p>{message}</p>
       {props.text}
