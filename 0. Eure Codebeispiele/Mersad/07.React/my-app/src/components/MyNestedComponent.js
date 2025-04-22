4fb833c88926203640f024c698b33cc31406d01b
Merso <mbesic@gmx.at>
2025-04-22 19:21:49 +0200

    merso

--- 0. Eure Codebeispiele/Mersad/07.React/my-app/src/components/MyNestedComponent.js
+++ 0. Eure Codebeispiele/Mersad/07.React/my-app/src/components/MyNestedComponent.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./style.css";
 
-export default function MyNestedComponent() {
-  return <div>Ich bin eine verschachtelte Komponente</div>;
+export default function MyNestedComponent(props) {
+  const name = "Mersad";
+  const message = `Hallo ${name}`;
+  return (
+    <div style={{ backgroundColor: "grey", color: props.color }}>
+      Ich bin eine verschachtelte Komponente
+      <p>{message}</p>
+      {props.text}
+      {props.textZwei}
+    </div>
+  );
 }
