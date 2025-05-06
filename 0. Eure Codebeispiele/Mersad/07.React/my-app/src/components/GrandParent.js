e3f6628ce3618f026bd35e72e36e0a4db7f616cd
Merso <mbesic@gmx.at>
2025-05-06 21:05:52 +0200

    merso

--- 0. Eure Codebeispiele/Mersad/07.React/my-app/src/components/GrandParent.js
+++ 0. Eure Codebeispiele/Mersad/07.React/my-app/src/components/GrandParent.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import MyParentComponent from "./MyParentComponent";
 
 export default function MyGrandComponent() {
+  const [count, setCount] = useState(0);
+  function handleClickPlus() {
+    let newCount = count + 1;
+    setCount(newCount);
+    console.log(count);
+  }
+  function handleClickMinus() {
+    let newCount = count - 1;
+    setCount(newCount);
+    console.log(count);
+  }
   return (
     <div>
       <MyParentComponent />
       <MyParentComponent />
+      <p>{count}</p>
+      <button onClick={handleClickMinus}> - </button>
+      <button onClick={handleClickPlus}> + </button>
     </div>
   );
 }
