020567a955e05e0d9d05a733607c547f3a8a2810
Merso <mbesic@gmx.at>
2025-05-13 20:00:14 +0200

    merso

---
+++ 0. Eure Codebeispiele/Mersad/07.React/lotto/src/components/generator/LottoGenerator.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+import LottoNumbers from "./LottoNumbers";
+
+export default function LottoGenerator() {
+  return (
+    <div>
+      <Header />
+      <LottoNumbers />
+    </div>
+  );
+}
