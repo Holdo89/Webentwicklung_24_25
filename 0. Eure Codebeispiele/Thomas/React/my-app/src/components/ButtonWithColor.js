5981692114ac8365c275b2a9be65d902f3e8ec7a
Thomas Meindlhumer <tom77654@example.com>
2025-05-20 16:13:50 +0200

    123

---
+++ 0. Eure Codebeispiele/Thomas/React/my-app/src/components/ButtonWithColor.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import  "./style.css"
+
+
+export default function MyButton(){
+  const[isClicked,setIsClicked]=useState("false")
+
+    function showAlert(){
+      //alert("Hello")
+      setIsClicked(!isClicked)
+    }
+
+  return(
+    <>
+    
+    <button style={{backgroundColor:isClicked ? "red":"blue"}} onClick={showAlert}>Alert
+      
+    </button>
+    </>
+  );
+}
